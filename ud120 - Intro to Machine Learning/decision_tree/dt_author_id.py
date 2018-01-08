22b133308b8e081642fb6f4803487d71ad1ca8a8
morrowind1983 <morrowind1983@gmail.com>
2018-01-08 13:53:26 +0800

    Add decision_tree

--- ud120 - Intro to Machine Learning/decision_tree/dt_author_id.py
+++ ud120 - Intro to Machine Learning/decision_tree/dt_author_id.py
@@ -24,8 +24,29 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+print "features:", len(features_train[0])
+
+clf = tree.DecisionTreeClassifier(criterion='entropy', min_samples_split=40)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t1, 3), "s"
+
+score = accuracy_score(labels_test, pred)
+print "accuracy score:", score
 
 #########################################################
 
-
+# no. of Chris training emails: 7936
+# no. of Sara training emails: 7884
+# features: 3785
+# training time: 21.138 s
+# predict time: 0.031 s
+# accuracy score: 0.973833902162
