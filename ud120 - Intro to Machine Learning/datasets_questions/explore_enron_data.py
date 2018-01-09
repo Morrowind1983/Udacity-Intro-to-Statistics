32b19f6266066fcb58b56f5c27bed7eb680fc375
morrowind1983 <morrowind1983@gmail.com>
2018-01-09 11:01:02 +0800

    Add datasets_questions

--- ud120 - Intro to Machine Learning/datasets_questions/explore_enron_data.py
+++ ud120 - Intro to Machine Learning/datasets_questions/explore_enron_data.py
@@ -19,4 +19,22 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print "There are", len(enron_data), "data points(i.e. people) in the Enron dataset"
+print "For each person,", len(enron_data.values()[0]), "features are available"
+print "There are", sum([value['poi'] for value in enron_data.values()]), "POIs in the dataset"
 
+print "The total value of the stock belonging to James Prentice is", enron_data["PRENTICE JAMES"]["total_stock_value"]
+print "We have", enron_data["COLWELL WESLEY"]["from_this_person_to_poi"], "messages from Wesley Colwell to POIs"
+print "The value of stock options exercised by Jeff Skilling is", enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+
+print "Total payments of Jeff Skilling is", enron_data["SKILLING JEFFREY K"]["total_payments"]
+print "Total payments of Kenneth Lay is", enron_data["LAY KENNETH L"]["total_payments"]
+print "Total payments of Andrew Fastow is", enron_data["FASTOW ANDREW S"]["total_payments"]
+
+print len([1 for value in enron_data.values() if value['salary'] != "NaN"]), "folks have a quantified salary"
+print len([1 for value in enron_data.values() if value['email_address'] != "NaN"]), "folks have a known email address"
+
+total_payments_nan = len([1 for value in enron_data.values() if value['total_payments'] == "NaN"])
+print round(100.0*total_payments_nan/len(enron_data)), "% of people in the dataset have 'NaN' for their total payments"
+total_payments_nan_poi = len([1 for value in enron_data.values() if value['poi'] and value['total_payments'] == "NaN"])
+print round(100.0*total_payments_nan_poi/len(enron_data)), "% of POIs in the dataset have 'NaN' for their total payments"
