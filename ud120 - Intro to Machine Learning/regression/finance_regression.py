f0318a0002902c96580fa8801b16ee4e65578768
morrowind1983 <morrowind1983@gmail.com>
2018-01-09 17:23:04 +0800

    Add finance_regression

--- ud120 - Intro to Machine Learning/regression/finance_regression.py
+++ ud120 - Intro to Machine Learning/regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,13 +37,15 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn import linear_model
 
-
-
-
-
-
-
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+score = reg.score(feature_train, target_train)
+print reg.coef_
+print reg.intercept_
+print reg.predict([[1]])
+print feature_test
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -62,6 +64,7 @@ plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 ### draw the regression line, once it's coded
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
+    #plt.plot( feature_train, reg.predict(feature_train) )
 except NameError:
     pass
 plt.xlabel(features_list[1])
